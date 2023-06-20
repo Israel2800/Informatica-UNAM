8f385c16dbdc26cd9caf8f2bbe4411f8237d982c
Dary <dady281100@gmail.com>
2023-06-19 23:13:22 -0500

    Challenge completed

--- Semestre-2/Introduccion-a-la-Programacion/numerosIguales.c
+++ Semestre-2/Introduccion-a-la-Programacion/numerosIguales.c
@@ -1,25 +1,25 @@
-// En este programa, se solicitan dos números del tipo entero
+// En este programa, se solicitan dos numeros del tipo entero
 // al usuario y se almacenan en las variables `numero1` y `numero2`.
 // Luego, se utiliza una estructura `if` para comparar si ambos
-// números son iguales usando el operador de igualdad (`==`). Por lo tanto,
-// dependiendo del resultado de la comparación, se imprime un  
-// mensaje indicando si los números son iguales o no.
+// numeros son iguales usando el operador de igualdad (`==`). Por lo tanto,
+// dependiendo del resultado de la comparacion, se imprime un  
+// mensaje indicando si los numeros son iguales o no.
 
 #include <stdio.h>
 
 int main() {
     int numero1, numero2;
 
-    printf("Ingrese el primer número: ");
+    printf("Ingrese el primer numero: ");
     scanf("%d", &numero1);
 
-    printf("Ingrese el segundo número: ");
+    printf("Ingrese el segundo numero: ");
     scanf("%d", &numero2);
 
     if (numero1 == numero2) {
-        printf("Los números son iguales.\n");
+        printf("Los numeros son iguales.\n");
     } else {
-        printf("Los números no son iguales.\n");
+        printf("Los numeros no son iguales.\n");
     }
 
     return 0;
